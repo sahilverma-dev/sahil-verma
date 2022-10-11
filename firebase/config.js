0a29ec5095c5f73c0aef6eb14e76876a82b0dcc0
Sahil Verma <domainsahil007@gmail.com>
2022-10-11 09:17:13 +0530

    google analytics added

--- firebase/config.js
+++ firebase/config.js
@@ -1,6 +1,7 @@
 import { initializeApp } from "firebase/app";
 
 import { getFirestore } from "firebase/firestore";
+import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
   apiKey: process?.env?.NEXT_PUBLIC_apiKey,
@@ -9,8 +10,10 @@ const firebaseConfig = {
   storageBucket: process?.env?.NEXT_PUBLIC_storageBucket,
   messagingSenderId: process?.env?.NEXT_PUBLIC_messagingSenderId,
   appId: process?.env?.NEXT_PUBLIC_appId,
+  measurementId: process?.env?.NEXT_PUBLIC_measurementId,
 };
 
 const app = initializeApp(firebaseConfig);
 const firestore = getFirestore(app);
+const analytics = getAnalytics(app);
 export { firestore };
